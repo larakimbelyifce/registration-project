7ec693eafa24a1154aa8ed77e1487471260f9fd4
Danilo de Sousa <cdanilodesousa@gmail.com>
2021-09-20 11:37:19 -0300

    creating new views

--- registration/website/urls.py
+++ registration/website/urls.py
@@ -1,14 +1,25 @@
 # Importação das funções definidas no arquivo views.py.
 from django.urls import path
-from website.views import IndexTemplateView, FuncionarioListView
+from website.views import IndexTemplateView, FuncionarioListView, FuncionarioUpdateView, FuncionarioDeleteView, FuncionarioCreateView
 
 app_name = 'website'
 
 # A urlpatterns contém a lista de roteamentos de URLs.
 urlpatterns = [
-    #GET /
+    # GET /
     path('', IndexTemplateView.as_view(), name="index"),
 
     # GET /funcionarios
     path('funcionarios/', FuncionarioListView.as_view(), name="lista_funcionarios"),
+
+    # Para atualizar o objeto, devemos buscá-lo via 'id' ou 'slug'. Também pode ser atualizado através do método get_object().
+    # GET/POST /funcionario/{pk do objeto}
+    path('funcionario/<pk>', FuncionarioUpdateView.as_view(), name = "atualiza_funcionario"),
+
+    # Para deletar o objeto, o procedimento é parecido com o de atualização.
+    # GET/POST /funcionario/excluir/{pk do objeto}
+    path('funcionario/excluir/<pk>', FuncionarioDeleteView.as_view(), name="deleta_funcionario"),
+
+    # GET /funcionario/cadastrar
+    path('funcionario/cadastrar', FuncionarioCreateView.as_view(), name="cadastra_funcionario"),
 ]
--- registration/website/views.py
+++ registration/website/views.py
@@ -1,12 +1,36 @@
-# Utilizando a ListView para a listagem de funcionários.
 from registration.models import Funcionario
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, UpdateView, DeleteView, CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+# Utilizando a TemplateView para rederização da página.
 class IndexTemplateView(TemplateView):
-    template_name = 'website/index.html'
+    template_name = "website/index.html"
 
+# Utilizando a ListView para a listagem de funcionários.
 class FuncionarioListView(ListView):
     template_name = "website/lista.html"
     model = Funcionario
-    context_object_name = "funcionarios"
+    context_object_name = 'funcionarios'
+
+# Utilizando a UpdateView para a atualização de funcionários.
+class FuncionarioUpdateView(UpdateView):
+    template_name = "website/atualiza.html"
+    model = Funcionario
+    fields = '__all__' # fields = ['nome', 'email', 'cpf', 'celular', 'rg', 'endereco', 'salario']
+    context_object_name = 'funcionario'
+    success_url = reverse_lazy("website:lista_funcionários")
+
+# Utilizando a DeleteView para deletar um funcionário.
+class FuncionarioDeleteView(DeleteView):
+    template_name = "website/exclui.html"
+    model = Funcionario
+    context_object_name = 'funcionario'
+    success_url = reverse_lazy("website:lista_funcionarios")
+
+# Utilizando a CreateView para adicionar um novo funcionário.
+class FuncionarioCreateView (CreateView):
+    template_name = "website/cria.html"
+    model = Funcionario
+    form_class = InsereFuncionarioForm
+    # O método reverse_lazy() vai traduzir a View em URL. Após adicionar um funcionário, haverá um redirecionamento para a página de listagem atualizada.
+    success_url = reverse_lazy("website:lista_funcionarios")
